c8dcbf997923f74b62abf194fb2b8c9a28ff6a73
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-30 04:07:43 +0200

    Added The card Wrapper Component

--- 01-starting-project/src/components/UI/Card.js
+++ 01-starting-project/src/components/UI/Card.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Card.module.css";
 
+//The card Wrapper Component
 const Card = (props) => {
   return (
     <div className={`${styles.card} ${props.className}`}>{props.children}</div>
